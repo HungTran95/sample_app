11119cfc7aba84647000d644bc0590deb7276c63
HungTran95 <hunglinh9599@gmail.com>
2020-05-21 00:23:21 -0700

    chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,20 @@ class User < ApplicationRecord
 
   before_save :downcase_email
 
+  def self.digest string
+    cost =  if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+    BCrypt::Password.create string, cost: cost
+  end
+
   private
 
   def downcase_email
     email.downcase!
   end
+
   has_secure_password
 end
