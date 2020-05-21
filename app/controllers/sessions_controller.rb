8ab67fd318efa01e87d810458b3fd5dcb9530621
HungTran95 <hunglinh9599@gmail.com>
2020-05-21 00:23:21 -0700

    Chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,21 @@
 class SessionsController < ApplicationController
+  def new; end
+
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       flash[:success] = t("static_pages.user.lgsuccess")
       log_in user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to user
     else
-      flash.now[:danger] = t("static_pages.home.invalid")
+      flash.now[:danger] = t "static_pages.home.invalid"
       render :new
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
-
-  def new; end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_save :downcase_email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   validates :email, presence: true,
@@ -10,7 +12,37 @@ class User < ApplicationRecord
   validates :password, presence: true,
     length: {maximum: Settings.pass_max_length}
 
-  before_save :downcase_email
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  has_secure_password
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 
   def self.digest string
     cost =  if ActiveModel::SecurePassword.min_cost
@@ -26,6 +58,4 @@ class User < ApplicationRecord
   def downcase_email
     email.downcase!
   end
-
-  has_secure_password
 end
